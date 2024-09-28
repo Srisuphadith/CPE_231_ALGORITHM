4fef7eaa596e45e740582b5873b7820572b03970
Nine <ninerattana2005@gmail.com>
2024-09-29 00:13:16 +0700

    merge_sort_complete

--- lab5/mergesort.cpp
+++ lab5/mergesort.cpp
@@ -1,34 +1,84 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
-void merge(int arr[],int k,int m,int n){
-    // k > m-1
-    // m > n-1
+void merge(int arr[], int y, int m, int n)
+{
+    int tmp[n - y];
+    int i = y;
+    int j = m;
+    int k = 0;
+    while (i < m && j < n)
+    {
+        if (arr[i] <= arr[j])
+        {
+            tmp[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            tmp[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+    if (i == m)
+    {
+        for (int p = k; p < n - y; p++)
+        {
+            tmp[p] = arr[j];
+            j++;
+        }
+    }
+    else
+    {
+        for (int p = k; p < n - y; p++)
+        {
+            tmp[p] = arr[i];
+            i++;
+        }
+    }
+    k = 0;
+    for (int p = y; p < n; p++)
+    {
+        arr[p] = tmp[k];
+        k++;
+    }
 }
 
-
-int main(){
+int main()
+{
     int n;
     cin >> n;
     int arr[n];
-    for(int i = 0; i<n;i++){
-        cin  >> arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    if(n<2){
+    if (n < 2)
+    {
         return 0;
     }
     int i = 1;
-    while(i < n){
+    while (i < n)
+    {
         int j = 0;
-        while (j<n-i)
+        while (j < n - i)
         {
-            if(n<j+(2*i)){
-
-            }else{
-
+            if (n < j + (2 * i))
+            {
+                merge(arr, j, j + i, n);
             }
-            j = j+2*i;
+            else
+            {
+                merge(arr, j, j + i, j + (2 * i));
+            }
+
+            j += 2 * i;
         }
-        i = i*2;
+        i *= 2;
     }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
