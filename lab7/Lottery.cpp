ae15ffaeb449fb044486fc5c1cdff50b58108836
Nine <ninerattana2005@gmail.com>
2024-10-16 13:47:13 +0700

    initial lottery

---
+++ lab7/Lottery.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+int m,n;
+cin >> m >> n;
+char lottery[m];
+char buy[n];
+vector<pair<char,int> > sh_table;
+
+for(int i = 0;i<m;i++){
+    cin >> lottery[i];
+}
+for(int i = 0;i<n;i++){
+    cin >> buy[i];
+}
+
+}
